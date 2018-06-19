1e8d4545ed0aa46ad5dfeb39f1198c283cda50e4
Chris Bartoli <cbartoli@umass.edu>
2018-06-19 14:39:22 -0400

    Initial project commit

---
+++ CR/fpga/vivado_examples/aes128_verilog/aes128_verilog.ip_user_files/sim_scripts/clk_wiz_0/ies/run.f
@@ -0,0 +1,14 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "F:/Xilinx/Vivado/2018.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "F:/Xilinx/Vivado/2018.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../../../common/ip/clk_wiz_0/clk_wiz_0_clk_wiz.v" \
+  "../../../../../../common/ip/clk_wiz_0/clk_wiz_0.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
